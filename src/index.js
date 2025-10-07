67e13fc81cbf05de6d24e1d60fa8a0e56a0f897f
FlagonDragon <195494654+FlagonDragon@users.noreply.github.com>
2025-10-07 03:28:49 -0300

    github test

--- src/index.js
+++ src/index.js
@@ -1,28 +1,18 @@
-
-
-import './styles.css';
-import addProject from './projects.js'
-import addTodo from './todos.js'
-import {addProjectConfirmListener, addTodoConfirmListener} from './addListeners.js'
-import writeContent from './DOM.js'
-import {storageFeedback, getStorage, setStorage} from './storage.js'
-
 window.myProjects = [];
 
 window.addTodoId = '';
+
+import './styles.css';
+import addProject from './projects.js'
+import {addProjectConfirmListener, addTodoConfirmListener} from './addListeners.js'
+import writeContent from './DOM.js'
+import {storageFeedback, getStorage} from './storage.js'
 
 storageFeedback('localStorage');
 
 getStorage();
 
-// addProject('Yoga');
-// addProject('Cooking');
-// addProject('Writing');
-// addProject('Singing');
-// addProject('Studying');
-// addTodo('Yoga','Stretch legs', '08/10','None','Low');
-// addTodo('Cooking','Prepare breakfast','11/10','None','High');
-// addTodo('Cooking','Bake sponge cake','11/10','None','Medium');
+console.log(myProjects);
 
 if (myProjects.length == 0) {
 
